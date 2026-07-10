2ada049c5a514fb477a4e23bc86ad31b5becbe6b
TipoTramquilo <luisalejandroochoa30@gmail.com>
2026-07-10 00:40:41 -0400

    chk de tabla den dim

--- postgres/db_setup/create_tables.sql
+++ postgres/db_setup/create_tables.sql
@@ -218,7 +218,8 @@ create table DIM_ESTADO_CONTRATO (
 create table DIM_EVALUACION_SERVICIO (
     sk_dim_evaluacion_servicio serial primary key,
     cod_evaluacion int not null, -- nk numérico
-    nb_descrip varchar(50) not null
+    nb_descrip varchar(50) not null,
+    CONSTRAINT chk_descrip_estado CHECK (descrip_estado IN ('Malo', 'Regular', 'Bueno', 'Muy Bueno', 'Excelente'))
 );
 
 -- dimensión siniestro
